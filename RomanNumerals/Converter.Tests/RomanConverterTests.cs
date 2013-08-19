2e99448bc8cf2465a46cfce30f970464c790e746
Chirdeep Tomar <chirdeep.tomar@gmail.com>
2013-08-19 14:33:04 +0100

    Validation working...added test

--- RomanNumerals/Converter.Tests/RomanConverterTests.cs
+++ RomanNumerals/Converter.Tests/RomanConverterTests.cs
@@ -17,6 +17,15 @@ namespace Converter.Tests
             Assert.AreEqual("I", result);
         }
 
+        [Test]
+        public void Converter_Throws_Error_On_Invalid_Input()
+        {
+            var validator = new RomanValidator();
+            var converter = new RomanConverter(validator);
+            var result = converter.Convert(0);
+            Assert.AreEqual("Invalid Input!!!", result);
+        }
+
         #endregion
     }
 }
--- RomanNumerals/Converter/Impl/RomanConverter.cs
+++ RomanNumerals/Converter/Impl/RomanConverter.cs
@@ -1,17 +1,50 @@
-﻿using Converter.Contracts;
+﻿using System;
+
+using Converter.Contracts;
+using Converter.Enums;
 
 namespace Converter.Impl
 {
     public class RomanConverter : IRomanConverter
     {
+        #region Constants and Fields
+
+        private readonly IRomanValidator _validator = null;
+
+        #endregion
+
+        #region Constructors and Destructors
+
         public RomanConverter(IRomanValidator validator)
         {
-
+            _validator = validator;
         }
+
+        #endregion
+
+        #region Implemented Interfaces
+
+        #region INumberConverter<string,int>
 
         public string Convert(int number)
         {
+            if (_validator.Validate(number))
+            {
+                return "Invalid Input!!!";
+            }
+            else
+            {
+                return ConvertToRoman(number);
+            }
+        }
+
+        private string ConvertToRoman(int number)
+        {
             return "I";
         }
+
+        #endregion
+
+        #endregion
     }
 }
