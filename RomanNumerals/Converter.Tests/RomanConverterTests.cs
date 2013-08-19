9eb5d3004dd6007ee065e30e5147afd5f6394e44
Chirdeep Tomar <chirdeep.tomar@gmail.com>
2013-08-19 13:57:37 +0100

    Adding first test

---
+++ RomanNumerals/Converter.Tests/RomanConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace Converter.Tests
+{
+    [TestFixture]
+    class RomanConverterTests
+    {
+        [Test]
+        public void Converter_Can_Convert_One()
+        {
+            RomanConverter converter = new RomanConverter();
+            var result = converter.Convert(1);
+            Assert.AreEqual("I", result);
+        }
+
+    }
+}
