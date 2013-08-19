552bec553feb15317199dc25d520d4e1fdccdea1
Chirdeep Tomar <chirdeep.tomar@gmail.com>
2013-08-19 14:18:09 +0100

    Added some refactoring for loose coupling

--- RomanNumerals/Converter.Tests/RomanConverterTests.cs
+++ RomanNumerals/Converter.Tests/RomanConverterTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +15,47 @@ namespace Converter.Tests
         [Test]
         public void Converter_Can_Convert_One()
         {
-            RomanConverter converter = new RomanConverter();
+            IRomanValidator validator = new RomanValidator();
+            IRomanConverter converter = new RomanConverter(validator);
             var result = converter.Convert(1);
             Assert.AreEqual("I", result);
         }
 
+    }
+
+    internal class RomanValidator : IRomanValidator
+    {
+        public bool Validate(int number)
+        {
+            return number <= 0;
+        }
+    }
+
+    internal interface INumberConverter<out T, in U>
+    {
+        T Convert(U number);
+    }
+
+    internal interface IRomanConverter : INumberConverter<string, int>
+    {
+
+    }
+
+    internal class RomanConverter : IRomanConverter
+    {
+        public RomanConverter(IRomanValidator validator)
+        {
+
+        }
+
+        public string Convert(int number)
+        {
+            return "I";
+        }
+    }
+
+    internal interface IRomanValidator
+    {
+        bool Validate(int number);
     }
 }
